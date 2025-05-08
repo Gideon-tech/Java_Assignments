005995eea6c6ec7aac9cd14d6d0b2f20e50115a4
Maku Gideon <paintingislife592@gmail.com>
2025-05-08 20:58:27 +0300

    first commit

---
+++ profile.java
@@ -0,0 +1,24 @@
+class profile {
+    public static void main(String args[]){
+        String name = "Maku Gideon";
+        String rollNumber = "012240334";
+        String course = "AI/ML";
+        String university = "ISBAT University Kampala";
+        String dateOfJoin = "10| 09| 2023";
+        String mobileNumber = "+256760325115";
+
+
+        System.out.println("\tSTUDENT DETAILS");
+
+        System.out.println("*****************************");
+        System.out.println("Name        :" + name);
+        System.out.println("Roll Number :" + rollNumber);
+        System.out.println("Course      :" + course);
+        System.out.println("University  :" + university);
+        System.out.println("Date Of Join:" + dateOfJoin);
+        System.out.println("Mobile      :" + mobileNumber);
+        System.out.println("*****************************");
+        System.out.println("\tThank you");
+
+    }
+}
